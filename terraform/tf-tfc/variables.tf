498e32ce6f5ab95ceb0c858c73b46b47f969d8b8
duhio <steve@cfg.sh>
2023-11-18 14:03:54 -0800

    feat: configure terraform cloud

---
+++ terraform/tf-tfc/variables.tf
@@ -0,0 +1,11 @@
+variable "terraform_cloud_token" {
+  type = string
+  description = "Terraform Cloud User Token"
+  sensitive = true
+}
+
+variable "github_pat" {
+  type = string
+  description = "GitHub Personal Access Token"
+  sensitive = true
+}
