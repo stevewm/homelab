c1dbc28e6a4ca4ac292c6a2654a0c410aa9d9411
duhio <steve@cfg.sh>
2023-11-18 15:24:32 -0800

    fix(tf): fix up TFC config

--- terraform/tf-tfc/variables.tf
+++ terraform/tf-tfc/variables.tf
@@ -1,9 +1,3 @@
-variable "terraform_cloud_token" {
-  type        = string
-  description = "Terraform Cloud User Token"
-  sensitive   = true
-}
-
 variable "github_pat" {
   type        = string
   description = "GitHub Personal Access Token"
