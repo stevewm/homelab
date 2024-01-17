b6644e414228c94016f355e65739aed04f31c146
duhio <34654219+duhio@users.noreply.github.com>
2024-01-17 09:39:02 -0800

    fix: bootstrap secret names

--- kubernetes/mini/bootstrap.sh
+++ kubernetes/mini/bootstrap.sh
@@ -5,14 +5,11 @@
 # flux install
 kubectl apply -f https://github.com/fluxcd/flux2/manifests/install?ref=v2.2.2
 
-# secrets
-declare -a secrets=("SOPS_AGE_KEY" "HOMELAB_REPO_GITHUB_TOKEN")
+# # secrets
+doppler run -p homelab -c prd --only-secrets SOPS_AGE_KEY --command 'kubectl create secret generic github \
+                                                                        --from-literal=age.agekey=$SOPS_AGE_KEY'
+doppler run -p homelab -c prd --only-secrets HOMELAB_REPO_GITHUB_TOKEN --command 'kubectl create secret generic github \
+                                                                        --from-literal=token=$HOMELAB_REPO_GITHUB_TOKEN'
 
-for secret_name in "${secrets[@]}"
-do
-    # Use the secret name in the Doppler command
-    doppler run -p homelab -c prd --only-secrets "$secret_name" --command "kubectl create secret generic github --from-literal=token=\$$secret_name"
-done
-
-# flux configuration
+# # flux
 kubectl apply --kustomize flux/config
