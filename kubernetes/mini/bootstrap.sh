92764fc0a86ea53aacc58a4e8e908b3e9369cf82
duhio <34654219+duhio@users.noreply.github.com>
2024-01-17 09:37:45 -0800

    feat: leaner bootstrap process

--- kubernetes/mini/bootstrap.sh
+++ kubernetes/mini/bootstrap.sh
@@ -2,12 +2,17 @@
 
 # bootstrap cluster
 
-# # flux install
-kubectl apply --kustomize bootstrap/
+# flux install
+kubectl apply -f https://github.com/fluxcd/flux2/manifests/install?ref=v2.2.2
 
-# # secrets
-sops --decrypt bootstrap/age-key.sops.yaml | kubectl apply -f -
-sops --decrypt bootstrap/github-key.sops.yaml | kubectl apply -f -
+# secrets
+declare -a secrets=("SOPS_AGE_KEY" "HOMELAB_REPO_GITHUB_TOKEN")
 
-# # flux
+for secret_name in "${secrets[@]}"
+do
+    # Use the secret name in the Doppler command
+    doppler run -p homelab -c prd --only-secrets "$secret_name" --command "kubectl create secret generic github --from-literal=token=\$$secret_name"
+done
+
+# flux configuration
 kubectl apply --kustomize flux/config
