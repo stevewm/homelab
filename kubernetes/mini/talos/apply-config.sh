24ce9f802146aedbf12c4d37236b5cfb98de6d4b
duhio <34654219+duhio@users.noreply.github.com>
2024-01-20 23:00:53 -0800

    fix: sort out metrics server hopefully

--- kubernetes/mini/talos/apply-config.sh
+++ kubernetes/mini/talos/apply-config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-talosctl apply-config --insecure --nodes 192.168.1.51 --file ./clusterconfig/mini-control01.yaml
-talosctl apply-config --insecure --nodes 192.168.1.52 --file ./clusterconfig/mini-control02.yaml
-talosctl apply-config --insecure --nodes 192.168.1.53 --file ./clusterconfig/mini-control03.yaml
-talosctl apply-config --insecure --nodes 192.168.1.54 --file./clusterconfig/mini-worker01.yaml
+talosctl apply-config --nodes 192.168.1.51 --file ./clusterconfig/mini-control01.yaml --insecure
+talosctl apply-config --nodes 192.168.1.52 --file ./clusterconfig/mini-control02.yaml --insecure
+talosctl apply-config --nodes 192.168.1.53 --file ./clusterconfig/mini-control03.yaml --insecure
+talosctl apply-config --nodes 192.168.1.54 --file ./clusterconfig/mini-worker01.yaml --insecure
