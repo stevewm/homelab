34caef05073bc58edca83a43f972e60a77ba99e5
duhio <34654219+duhio@users.noreply.github.com>
2024-01-20 21:49:19 -0800

    feat(talos): new nodes

--- kubernetes/mini/talos/apply-config.sh
+++ kubernetes/mini/talos/apply-config.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-talosctl --insecure --nodes 192.168.1.51 ./talosconfig/mini-control01.yaml
+
+talosctl apply-config --insecure --nodes 192.168.1.51 --file ./clusterconfig/mini-control01.yaml
+talosctl apply-config --insecure --nodes 192.168.1.52 --file ./clusterconfig/mini-control02.yaml
+talosctl apply-config --insecure --nodes 192.168.1.53 --file ./clusterconfig/mini-control03.yaml
+talosctl apply-config --insecure --nodes 192.168.1.54 --file./clusterconfig/mini-worker01.yaml
