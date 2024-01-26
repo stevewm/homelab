81a6042a2bc319ef5e39deb046bc57be74c0bace
duhio <34654219+duhio@users.noreply.github.com>
2024-01-25 19:57:13 -0800

    fix: better bootstrapping

--- kubernetes/mini/bootstrap.sh
+++ kubernetes/mini/bootstrap.sh
@@ -2,14 +2,28 @@
 
 # bootstrap cluster
 
-# # flux install
-kubectl apply -f https://github.com/fluxcd/flux2/manifests/install?ref=v2.2.2
+## bootstrap flux
+echo "Bootstrapping flux, takes a little while to download the manifests..."
+mkdir -p bootstrap/
+VERSION=$(yq e 'select(documentIndex == 0) | .spec.ref.tag' flux/config/flux.yaml)
+cat <<EOF > bootstrap/kustomization.yaml
+---
+apiVersion: kustomize.config.k8s.io/v1beta1
+kind: Kustomization
+resources:
+  - github.com/fluxcd/flux2/manifests/install?ref=$VERSION
+EOF
 
-# # secrets
+kubectl apply --kustomize ./bootstrap/
+rm -rf bootstrap/
+
+
+# ## secrets
 doppler run -p homelab -c prd --only-secrets SOPS_AGE_KEY --command 'kubectl create secret generic github \
-                                                                        --from-literal=age.agekey=$SOPS_AGE_KEY'
-doppler run -p homelab -c prd --only-secrets HOMELAB_REPO_GITHUB_TOKEN --command 'kubectl create secret generic github \
-                                                                        --from-literal=token=$HOMELAB_REPO_GITHUB_TOKEN'
+                                                                         --from-literal=age.agekey=$SOPS_AGE_KEY'
 
-# # flux
+doppler run -p homelab -c prd --only-secrets HOMELAB_REPO_GITHUB_TOKEN --command 'kubectl create secret generic github \
+                                                                                    --from-literal=token=$HOMELAB_REPO_GITHUB_TOKEN'
+
+## flux
 kubectl apply --kustomize flux/config
