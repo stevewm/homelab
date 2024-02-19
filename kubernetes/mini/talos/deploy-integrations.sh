0420d949902f293bca97599ad5e17f2c9d4422b6
duhio <34654219+duhio@users.noreply.github.com>
2024-02-18 19:26:47 -0800

    feat: rename 'infrastructure' to 'core'

--- kubernetes/mini/talos/deploy-integrations.sh
+++ kubernetes/mini/talos/deploy-integrations.sh
@@ -8,13 +8,13 @@ pushd integrations >/dev/null 2>&1 || exit 1
 K8S_ROOT=$(git rev-parse --show-toplevel)/kubernetes/${CLUSTER_NAME}
 
 rm -rf cni/charts
-envsubst < $K8S_ROOT/infrastructure/kube-system/cilium/app/values.yaml > cni/values.yaml
+envsubst < $K8S_ROOT/core/kube-system/cilium/app/values.yaml > cni/values.yaml
 kustomize build --enable-helm cni | kubectl apply -f -
 rm cni/values.yaml
 rm -rf cni/charts
 
 rm -rf kubelet-csr-approver/charts
-envsubst < $K8S_ROOT/infrastructure/system/kubelet-csr-approver/app/values.yaml > kubelet-csr-approver/values.yaml
+envsubst < $K8S_ROOT/core/system/kubelet-csr-approver/app/values.yaml > kubelet-csr-approver/values.yaml
 if ! kubectl get ns system >/dev/null 2>&1; then
   kubectl create ns system
 fi
