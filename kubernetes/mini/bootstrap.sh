54e776a253f8f28689f6ccc888c49406f40e30ad
duhio <34654219+duhio@users.noreply.github.com>
2024-05-10 15:56:08 -0700

    feat(bootstrap): bootstrap cluster vars

--- kubernetes/mini/bootstrap.sh
+++ kubernetes/mini/bootstrap.sh
@@ -2,9 +2,17 @@
 
 # bootstrap cluster
 
-## bootstrap flux
 jp2a $(git rev-parse --show-toplevel)/docs/logo.png --colors --term-fit --fill --clear
 
+## secrets
+### doppler api token
+doppler run -p homelab -c prd --only-secrets DOPPLER_API_TOKEN --command 'kubectl create secret generic doppler \
+                                                  --from-literal=token=$DOPPLER_API_TOKEN --namespace=flux-system'
+## config
+### cluster variables
+kubectl apply -f ./flux/vars.yaml
+
+## bootstrap flux
 mkdir -p bootstrap/
 VERSION=$(yq e 'select(documentIndex == 0) | .spec.ref.tag' flux/config/flux.yaml)
 cat <<EOF > bootstrap/kustomization.yaml
@@ -14,15 +22,8 @@ kind: Kustomization
 resources:
   - github.com/fluxcd/flux2/manifests/install?ref=$VERSION
 EOF
-
 gum spin --spinner line --title "Bootstrapping Flux... this may take a while" -- kubectl apply --kustomize ./bootstrap/
 rm -rf bootstrap/
-
-
-## secrets
-### Doppler API Token
-doppler run -p homelab -c prd --only-secrets DOPPLER_API_TOKEN --command 'kubectl create secret generic doppler \
-                                                  --from-literal=token=$DOPPLER_API_TOKEN --namespace=flux-system'
 
 ## flux
 kubectl apply --kustomize flux/config
