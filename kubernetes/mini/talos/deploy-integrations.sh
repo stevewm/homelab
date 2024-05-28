73f1f13d1411ff0e814fcfcce08406d20f312414
duhio <34654219+duhio@users.noreply.github.com>
2024-05-27 19:19:30 -0700

    fix(talos): integrations filename

--- kubernetes/mini/talos/deploy-integrations.sh
+++ kubernetes/mini/talos/deploy-integrations.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-helmfile apply --skip-diff-on-install --suppress-diff --file talos/integrations/helmfile.yaml
+helmfile apply --skip-diff-on-install --suppress-diff --file integrations.yaml
