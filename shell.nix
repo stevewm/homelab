0d6377ccb89943e4273eb89510c9e7ad0047f43e
stevewm <34654219+stevewm@users.noreply.github.com>
2024-06-22 12:10:22 -0700

    chore: get rid of devshell

--- shell.nix
+++
@@ -1,6 +0,0 @@
-{ system ? builtins.currentSystem }:
-let
-  src = fetchTarball "https://github.com/numtide/devshell/archive/main.tar.gz";
-  devshell = import src { inherit system; };
-in
-devshell.fromTOML ./devshell.toml
