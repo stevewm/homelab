5b4f81ecb91b01d694ae71409dbb1a05537637c9
stevewm <34654219+stevewm@users.noreply.github.com>
2024-09-29 19:43:52 -0700

    chore: clean up remaining worker01 stuff

--- kubernetes/mini/talos/apply-config.sh
+++ kubernetes/mini/talos/apply-config.sh
@@ -3,4 +3,3 @@
 talosctl apply-config --nodes 192.168.1.51 --file ./clusterconfig/mini-control01.yaml --insecure
 talosctl apply-config --nodes 192.168.1.52 --file ./clusterconfig/mini-control02.yaml --insecure
 talosctl apply-config --nodes 192.168.1.53 --file ./clusterconfig/mini-control03.yaml --insecure
-talosctl apply-config --nodes 192.168.1.54 --file ./clusterconfig/mini-worker01.yaml --insecure
