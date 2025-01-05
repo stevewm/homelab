23e34c9c39d7067916f22c58ffb70b9366697e8d
stevewm <34654219+stevewm@users.noreply.github.com>
2025-01-05 11:09:06 +0100

    chore: tidy up a bit

--- kubernetes/main/bootstrap.sh
+++ kubernetes/main/bootstrap.sh
@@ -2,16 +2,12 @@
 
 # bootstrap cluster
 
-jp2a $(git rev-parse --show-toplevel)/docs/logo.png --colors --term-fit --fill --clear
+jp2a "$(git rev-parse --show-toplevel)/docs/logo.png --colors --term-fit --fill --clear"
 
 ## secrets
 ### doppler api token
-doppler run -p homelab -c prd --only-secrets DOPPLER_API_TOKEN --command 'kubectl create secret generic doppler \
-                                                  --from-literal=token=$DOPPLER_API_TOKEN --namespace=flux-system'
-## config
-### cluster variables
-kubectl apply -f ./flux/vars.yaml
-
+doppler run -p homelab -c prd --only-secrets DOPPLER_API_TOKEN --command "kubectl create secret generic doppler \
+                                                  --from-literal=token=$DOPPLER_API_TOKEN --namespace=flux-system"
 ## bootstrap flux
 mkdir -p bootstrap/
 VERSION=$(yq e 'select(documentIndex == 0) | .spec.ref.tag' flux/config/flux.yaml)
@@ -22,6 +18,7 @@ kind: Kustomization
 resources:
   - github.com/fluxcd/flux2/manifests/install?ref=$VERSION
 EOF
+
 gum spin --spinner line --title "Bootstrapping Flux... this may take a while" -- kubectl apply --kustomize ./bootstrap/
 rm -rf bootstrap/
 
