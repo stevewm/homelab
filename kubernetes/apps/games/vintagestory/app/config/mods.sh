18bb5b956df16d306aec2af47145581daaff45a3
stevewm <34654219+stevewm@users.noreply.github.com>
2025-02-06 20:44:24 +0100

    use sh

--- kubernetes/apps/games/vintagestory/app/config/mods.sh
+++ kubernetes/apps/games/vintagestory/app/config/mods.sh
@@ -1,23 +1,22 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 set -e
 
 mkdir -p "$MODS_DIR"
 
-urls=(
-  "https://mods.vintagestory.at/download?fileid=29227" # Primitive Survival v3.7.5
-  "https://mods.vintagestory.at/download?fileid=31297" # Expanded Foods v1.7.3
-  "https://mods.vintagestory.at/download?fileid=31302" # A Culinary Artillery v1.2.3
-  "https://mods.vintagestory.at/download?fileid=26136" # CarryOn v1.8.0-pre.1
-  "https://mods.vintagestory.at/download?fileid=30660" # BetterRuins v0.4.6
-  "https://mods.vintagestory.at/download?fileid=31137" # ConfigLib v1.4.4
-  "https://mods.vintagestory.at/download?fileid=26632" # ImGui v1.1.7
-  "https://mods.vintagestory.at/download?fileid=30915" # Ancient Dungeons (Th3Dungeon) v0.4.0
-)
+urls="\
+https://mods.vintagestory.at/download?fileid=29227 \
+https://mods.vintagestory.at/download?fileid=31297 \
+https://mods.vintagestory.at/download?fileid=31302 \
+https://mods.vintagestory.at/download?fileid=26136 \
+https://mods.vintagestory.at/download?fileid=30660 \
+https://mods.vintagestory.at/download?fileid=31137 \
+https://mods.vintagestory.at/download?fileid=26632 \
+https://mods.vintagestory.at/download?fileid=30915"
 
 rm -rf "${MODS_DIR:?}"/*
 cd "$MODS_DIR"
-for url in "${urls[@]}"; do
+for url in $urls; do
   curl -LOJs "$url"
 done
 
