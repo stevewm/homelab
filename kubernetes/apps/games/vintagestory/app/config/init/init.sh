3cfbbd66ee5ee596d4fa1e2750c88aaad209c34b
steve <34654219+stevewm@users.noreply.github.com>
2025-04-06 19:39:57 +0100

    feat(vs): new mod downloader (#1092)

---
+++ kubernetes/apps/games/vintagestory/app/config/init/init.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+set -e
+
+MODS_DIR="${VS_DATA_PATH:?}/Mods"
+# shellcheck disable=SC2034 # used in mods.py
+MODS_FILE="/init/mods.yaml"
+
+download_mods() {
+  mkdir -p "$MODS_DIR"
+
+  uv run --script mods.py
+}
+
+copy_directories() {
+  for dir in /tmp/*/; do
+    cp -r "$dir" "$VS_DATA_PATH/"
+    echo "Copying $dir to $VS_DATA_PATH"
+  done
+
+  echo "Finished copying from $dir"
+}
+
+download_mods
+copy_directories
