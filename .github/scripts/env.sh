49a720b6ed18aa75b4143a2f3d4c7e9e1b71c1ba
stevewm <34654219+stevewm@users.noreply.github.com>
2025-07-23 11:29:51 +0200

    chore: clean up lychee leftovers

--- .github/scripts/env.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-if [ ! -f ./.env.txt ]; then
-    doppler secrets get GITHUB_HOMELAB_TOKEN --plain > ./env.txt
-fi
-
-GITHUB_TOKEN=$(cat .env.txt)
-export GITHUB_TOKEN
