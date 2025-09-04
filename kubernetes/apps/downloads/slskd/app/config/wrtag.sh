32a36b2bcc5117a870b4526ae9249b39122f5860
stevewm <34654219+stevewm@users.noreply.github.com>
2025-09-04 11:28:03 +0200

    fix(slskd): wrtag http

--- kubernetes/apps/downloads/slskd/app/config/wrtag.sh
+++ kubernetes/apps/downloads/slskd/app/config/wrtag.sh
@@ -5,4 +5,4 @@ INPUT_PATH=$(echo "${SLSKD_SCRIPT_DATA}" | jq -r .localDirectoryName)
 echo "Starting import of folder from: ${INPUT_PATH}" >> /logs/post_download_log.txt
 
 # wget is the only thing available in the slskd container
-wget --post-data "path=${INPUT_PATH}" "https://'':${WRTAG_API_KEY}@${WRTAG_URL}/op/move"
+wget --post-data "path=${INPUT_PATH}" "http://'':${WRTAG_API_KEY}@${WRTAG_URL}/op/move"
